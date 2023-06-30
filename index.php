0f330413eed9e8290eeb5af140b6914294d5dbca
Alecpu92 <alessandro_belfiore@hotmail.it>
2023-06-30 17:29:50 +0200

    creazione classe,istanza,funzione,costruttore

--- index.php
+++ index.php
@@ -1,1 +1,30 @@
-<?php Hello World ?>
+<?php
+
+class Movie {
+
+    public $name;
+    public $genre;
+    public $director;
+
+    public function getFullOptions() {
+
+        return $this -> name . "" . $this -> genre . "" . $this -> director;
+    }
+
+}
+
+$movie1 = new Movie();
+$movie1 -> name = "Django";
+$movie1 -> genre= "Action";
+$movie1 -> director = "Tarantino";
+
+$movie2 = new Movie();
+$movie2 -> name = "Venom";
+$movie2 -> genre= "Superhero";
+$movie2 -> director = "Serkis";
+
+echo $movie1 -> getFullOptions();
+
+echo "<br><br>";
+
+echo $movie2 -> getFullOptions();
