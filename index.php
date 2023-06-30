2ed65e79675787bcd4f2a99baf7ce679116d0269
Alecpu92 <alessandro_belfiore@hotmail.it>
2023-06-30 17:44:01 +0200

    construct

--- index.php
+++ index.php
@@ -6,6 +6,13 @@ class Movie {
     public $genre;
     public $director;
 
+    public function __construct($name, $genre, $director) {
+
+        $this -> name = $name;
+        $this -> genre = $genre;
+        $this -> director = $director;
+    }
+
     public function getFullOptions() {
 
         return $this -> name . "" . $this -> genre . "" . $this -> director;
@@ -13,15 +20,19 @@ class Movie {
 
 }
 
-$movie1 = new Movie();
-$movie1 -> name = "Django";
-$movie1 -> genre= "Action";
-$movie1 -> director = "Tarantino";
 
-$movie2 = new Movie();
-$movie2 -> name = "Venom";
-$movie2 -> genre= "Superhero";
-$movie2 -> director = "Serkis";
+$movie1 = new Movie("Django", "Action", "Tarantino");
+// $movie1 = new Movie();
+// $movie1 -> name = "Django";
+// $movie1 -> genre= "Action";
+// $movie1 -> director = "Tarantino";
+
+
+$movie2 = new Movie("Venom", "Superhero", "Serkis");
+// $movie2 = new Movie();
+// $movie2 -> name = "Venom";
+// $movie2 -> genre= "Superhero";
+// $movie2 -> director = "Serkis";
 
 echo $movie1 -> getFullOptions();
 
